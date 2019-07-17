64473b9ecc7b96d30c9b83cf012077b5e75dc7f3
geekerstar <247507792@qq.com>
2019-07-17 14:20:02 +0800

    Return返回消息

--- rabbitmq-api/src/main/java/com/geekerstar/rabbitmq/api/returnlistener/Consumer.java
+++ rabbitmq-api/src/main/java/com/geekerstar/rabbitmq/api/returnlistener/Consumer.java
@@ -1,4 +1,4 @@
-package com.geekerstar.rabbitmq.api.confirm;
+package com.geekerstar.rabbitmq.api.returnlistener;
 
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
@@ -12,39 +12,32 @@ public class Consumer {
 	public static void main(String[] args) throws Exception {
 
 
-		//1 创建ConnectionFactory
 		ConnectionFactory connectionFactory = new ConnectionFactory();
 		connectionFactory.setHost("192.168.11.76");
 		connectionFactory.setPort(5672);
 		connectionFactory.setVirtualHost("/");
 
-		//2 获取C	onnection
 		Connection connection = connectionFactory.newConnection();
-
-		//3 通过Connection创建一个新的Channel
 		Channel channel = connection.createChannel();
 
-		String exchangeName = "test_confirm_exchange";
-		//* 匹配一个，# 匹配多个
-		String routingKey = "confirm.#";
-		String queueName = "test_confirm_queue";
+		String exchangeName = "test_return_exchange";
+		String routingKey = "return.#";
+		String queueName = "test_return_queue";
 
-		//4 声明交换机和队列 然后进行绑定设置, 最后指定路由Key
-		channel.exchangeDeclare(exchangeName, "topic", true);
+		channel.exchangeDeclare(exchangeName, "topic", true, false, null);
 		channel.queueDeclare(queueName, true, false, false, null);
 		channel.queueBind(queueName, exchangeName, routingKey);
 
-		//5 创建消费者
 		QueueingConsumer queueingConsumer = new QueueingConsumer(channel);
+
 		channel.basicConsume(queueName, true, queueingConsumer);
 
 		while(true){
+
 			Delivery delivery = queueingConsumer.nextDelivery();
 			String msg = new String(delivery.getBody());
-
-			System.err.println("消费端: " + msg);
+			System.err.println("消费者: " + msg);
 		}
-
 
 	}
 }
